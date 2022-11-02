2e8f8a16261af63c6d0126c6073d6143cd768a07
David Madner <david.madner@gmail.com>
2022-11-02 12:06:55 +0100

    try to revalidate every hour

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,11 @@ const weekdayStrings = [
   "Freitag",
 ];
 
+export const revalidate = 3600;
+
 const getMenu = async () => {
   const dataBuffer = await fetch(
-    "http://www.restaurant-marienhof.at/restaurant/pdf/wochenmenue.pdf",
-    {
-      next: {
-        revalidate: 60,
-      },
-    }
+    "http://www.restaurant-marienhof.at/restaurant/pdf/wochenmenue.pdf"
   );
 
   const blobContent = await dataBuffer.arrayBuffer();
