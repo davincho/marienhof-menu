a24c8d6e95ac7d8329052c7fe443623847363e64
David Madner <david.madner@gmail.com>
2022-12-01 20:31:46 +0100

    activate rewrites for subdomains

---
+++ app/marie/page.tsx
@@ -0,0 +1,40 @@
+import { marienRenderer } from "../render";
+
+import parser from "./../../utils/marify.parse";
+import HomePage from "./../HomePage";
+import pdf from "./../pdfShim";
+
+const getMenu = async () => {
+  const dataBuffer = await fetch(
+    "http://www.restaurant-marienhof.at/restaurant/pdf/wochenmenue.pdf",
+    { next: { revalidate: 3600 } }
+  );
+
+  const blobContent = await dataBuffer.arrayBuffer();
+
+  const data = (await pdf(Buffer.from(blobContent))) as { text: string };
+
+  const { days, weekDateRange } = parser(data.text);
+
+  return {
+    days,
+    weekDateRange,
+    timestamp: new Date().toLocaleString("de", { timeZone: "Europe/Vienna" }),
+  };
+};
+
+export default async function Page() {
+  const { days, timestamp, weekDateRange } = await getMenu();
+
+  return (
+    <HomePage
+      title="Marienhof Menu"
+      emoji="🧑🏼‍🍳"
+      telNumber="+431408890530"
+      days={days}
+      weekDateRange={weekDateRange}
+      timestamp={timestamp}
+      menuRenderer={marienRenderer}
+    />
+  );
+}
--- next.config.js
+++ next.config.js
@@ -5,6 +5,28 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  rewrites: async () => {
+    return [
+      {
+        has: [
+          {
+            type: "host",
+            value: "paulify.vercel.app",
+          },
+        ],
+        destination: "/paul",
+      },
+      {
+        has: [
+          {
+            type: "host",
+            value: "marinify.vercel.app",
+          },
+        ],
+        destination: "/marie",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
