08ae1f7b517349035b82e35059d17f6bf926820c
David Madner <david.madner@gmail.com>
2022-12-01 21:57:51 +0100

    change color tokens

--- app/render.tsx
+++ app/render.tsx
@@ -4,7 +4,7 @@ const TYPE_MAPPING = {
   INTERNATIONAL: "text-amber-900 bg-amber-300",
   SUPPE: "text-emerald-900 bg-emerald-300",
   "WIENER KÜCHE": "text-fuchsia-900 bg-fuchsia-300",
-  PASTA: "text-amber-900 bg-amber-300",
+  PASTA: "text-violet-900 bg-violet-300",
   FISCH: "text-blue-900 bg-blue-300",
 };
 
@@ -12,7 +12,7 @@ export const paulRenderer = ([type, name, price]: string[]) => (
   <>
     <div>
       <span
-        className={`text- inline-block text-xs rounded-md p-1 mr-1 ${
+        className={`inline-block text-xs rounded-md p-1 mr-1 ${
           TYPE_MAPPING[type as keyof typeof TYPE_MAPPING]
         }`}
       >
