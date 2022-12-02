3801b081ed4bc7c7dc6fd30c523893e613dffebd
David Madner <david.madner@gmail.com>
2022-12-02 09:23:21 +0100

    fix edge function

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,25 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+const VALID_DOMAINS = new Map<string, string>([
+  ["paulify.vercel.app", "/paul"],
+  ["marienify.vercel.app", "/marie"],
+]);
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === "/") {
     // Make subdomain redirects manually here, as redirect/rewrite functionality via
     // next.config.js does not work with root URLs 🤷🏽
 
-    const currentUrl = new URL(request.url);
+    const { hostname } = new URL(request.url);
 
-    if (currentUrl.hostname === "paulify.vercel.app") {
-      return NextResponse.redirect(new URL("/paul", "iamhungry.vercel.app"));
-    }
+    const redirectUrl = VALID_DOMAINS.get(hostname);
 
-    if (currentUrl.hostname === "marienify.vercel.app") {
-      return NextResponse.redirect(new URL("/marie", "iamhungry.vercel.app"));
+    if (redirectUrl) {
+      return NextResponse.redirect(
+        new URL(redirectUrl, "https://iamhungry.vercel.app")
+      );
     }
   }
 }
