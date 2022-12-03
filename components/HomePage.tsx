b7e85be76416f98b89c7ccb31133e1adac21c290
David Madner <david.madner@gmail.com>
2022-12-03 17:06:26 +0100

    fix mobile

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -46,7 +46,7 @@ const Home: NextPage<{
       <div className="md:w-3/4 mx-auto mb-8">
         <div className="mb-2 md:mb-8 relative grid grid-rows-[42px_1fr] gap-2 md:grid-rows-none md:grid-cols-[80px_1fr_80px]">
           <Link
-            className="border rounded-md p-2 border-gray-200 hover:border-gray-500 inline-block w-[80px]"
+            className="border rounded-md p-2 border-gray-200 hover:border-gray-500 inline-block w-[100px] text-center"
             href={isProduction ? "https://iamhungry.vercel.app" : "/"}
           >
             👈🏻 back
