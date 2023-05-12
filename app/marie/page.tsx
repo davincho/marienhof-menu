5fa6fc43737c55f02dfbf0fb19d31130cb10612e
David Madner <david.madner@gmail.com>
2023-05-12 11:47:41 +0200

    we need cookies in layout

--- app/marie/page.tsx
+++ app/marie/page.tsx
@@ -5,8 +5,6 @@ import parser from "./../../utils/marify.parse";
 import pdf from "./../pdfShim";
 
 const getMenu = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
   const dataBuffer = await fetch(
     "http://www.restaurant-marienhof.at/restaurant/pdf/wochenmenue.pdf",
     { next: { revalidate: 3600 } }
