43d71e3e964318d7be82922f84ab659a50d8bdd3
David Madner <david.madner@gmail.com>
2023-08-04 15:54:13 +0200

    fix: middleware slowed down the whole page, rely on domain redirects and check referer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import Link from "next/link";
 
 const commonLinkStyle =
   "border-gray-200 hover:border-gray-400 border-2 text-4xl rounded-md p-2 w-full text-center";
 
+const VALID_DOMAINS = new Map<string, string>([
+  ["paulify.vercel.app", "/paul"],
+  ["marienify.vercel.app", "/marie"],
+  ["limonis.vercel.app", "/limonis"],
+]);
+
 export default async function Page() {
+  const headersList = headers();
+  const referer = headersList.get("referer");
+
+  if (referer) {
+    const { hostname } = new URL(referer);
+
+    const redirectUrl = VALID_DOMAINS.get(hostname);
+
+    if (redirectUrl) {
+      redirect(redirectUrl);
+    }
+  }
+
   return (
     <div className="container mx-auto h-screen p-4">
       <div className="flex h-full items-center justify-center">
